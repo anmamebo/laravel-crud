010fae230d6b521a130b8a8342e558f1345a9e18
Antonio Manuel <anmamebo2001@gmail.com>
2023-03-17 12:19:21 +0100

    Añadido bootstrap y sass con vite al proyecto

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>EPD3 - TAD</title>
+
+    @vite(['resources/js/app.js', 'resources/css/app.scss'])
 </head>
 
 <body>
