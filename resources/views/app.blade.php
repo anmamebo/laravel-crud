092628019855bd645b1022aa555a19ad6fcd64ca
Antonio Manuel <anmamebo2001@gmail.com>
2023-03-18 13:50:32 +0100

    Creada vista contenedora con header, sidebar y footer y añadidos estilos a app.scss

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,21 +5,82 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>EPD3 - TAD</title>
+    <title>CRUD - Laravel</title>
 
     @vite(['resources/js/app.js', 'resources/css/app.scss'])
 </head>
 
 <body>
+    <!-- HEADER -->
+    <header class="navbar navbar-expand-md navbar-dark bg-primary sticky-top p-2 border-bottom">
+        <a class="navbar-brand px-3 text-white" href="{{ route('trabajadores') }}">CRUD LARAVEL</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+    </header>
 
-    <!-- CONTENIDO ESTÁTICO PARA TODAS LAS SECCIONES -->
-    <h1>EPD3 OPERACIONES CRUD.</h1>
+    <!-- CUERPO -->
+    <div class="main-container container-fluid">
+        <div class="row">
+            <!-- SIDEBAR -->
+            <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-dark sidebar collapse">
+                <div class="position-sticky pt-3 sidebar-sticky">
+                    <h6 class="sidebar-heading text-white px-3 mt-5 mb-4 text-uppercase">
+                        <span>Gestión Trabajadores</span>
+                    </h6>
+                    <ul class="nav flex-column">
+                        <li class="nav-item">
+                            <a class="nav-link text-white" aria-current="page" href="{{ route('trabajadores') }}">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="me-2 ºfeather feather-plus-circle align-text-bottom" width="24" height="24" fill="currentColor" class="bi bi-people" viewBox="0 0 16 16">
+                                    <path d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1h8Zm-7.978-1A.261.261 0 0 1 7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002a.274.274 0 0 1-.014.002H7.022ZM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4Zm3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0ZM6.936 9.28a5.88 5.88 0 0 0-1.23-.247A7.35 7.35 0 0 0 5 9c-4 0-5 3-5 4 0 .667.333 1 1 1h4.216A2.238 2.238 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816ZM4.92 10A5.493 5.493 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275ZM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0Zm3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4Z" />
+                                </svg>
+                                Trabajadores
+                            </a>
+                        </li>
+                        <li class="nav-item d-flex justify-content-between align-items-center me-4">
+                            <a class="nav-link text-white" href="{{ route('empleados') }}">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="me-2 feather feather-plus-circle align-text-bottom" width="24" height="24" fill="currentColor" class="bi bi-people" viewBox="0 0 16 16">
+                                    <path d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1h8Zm-7.978-1A.261.261 0 0 1 7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002a.274.274 0 0 1-.014.002H7.022ZM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4Zm3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0ZM6.936 9.28a5.88 5.88 0 0 0-1.23-.247A7.35 7.35 0 0 0 5 9c-4 0-5 3-5 4 0 .667.333 1 1 1h4.216A2.238 2.238 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816ZM4.92 10A5.493 5.493 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275ZM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0Zm3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4Z" />
+                                </svg>
+                                Empleados
+                            </a>
+                            <a class="link-secondary" href="{{ route('empleados.create') }}" aria-label="Añadir nuevo empleado">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus-circle align-text-bottom" aria-hidden="true">
+                                    <circle cx="12" cy="12" r="10"></circle>
+                                    <line x1="12" y1="8" x2="12" y2="16"></line>
+                                    <line x1="8" y1="12" x2="16" y2="12"></line>
+                                </svg>
+                            </a>
+                        </li>
+                        <li class="nav-item d-flex justify-content-between align-items-center me-4">
+                            <a class="nav-link text-white" href="{{ route('gerentes') }}">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="me-2 feather feather-plus-circle align-text-bottom" width="24" height="24" fill="currentColor" class="bi bi-people" viewBox="0 0 16 16">
+                                    <path d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1h8Zm-7.978-1A.261.261 0 0 1 7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002a.274.274 0 0 1-.014.002H7.022ZM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4Zm3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0ZM6.936 9.28a5.88 5.88 0 0 0-1.23-.247A7.35 7.35 0 0 0 5 9c-4 0-5 3-5 4 0 .667.333 1 1 1h4.216A2.238 2.238 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816ZM4.92 10A5.493 5.493 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275ZM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0Zm3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4Z" />
+                                </svg>
+                                Gerentes
+                            </a>
+                            <a class="link-secondary" href="{{ route('gerentes.create') }}" aria-label="Añadir nuevo gerente">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus-circle align-text-bottom" aria-hidden="true">
+                                    <circle cx="12" cy="12" r="10"></circle>
+                                    <line x1="12" y1="8" x2="12" y2="16"></line>
+                                    <line x1="8" y1="12" x2="16" y2="12"></line>
+                                </svg>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
 
-    <!-- CONTENIDO DINÁMICO EN FUNCIÓN DE LA SECCIÓN QUE SE VISITA -->
-    <div>
-        @yield('apartado')
+            <!-- CONTENIDO PRINCIPAL -->
+            <main class="principal-content-container bg-dark col-md-9 ms-sm-auto col-lg-10 px-md-4 py-4">
+                @yield('apartado')
+            </main>
+        </div>
     </div>
 
+    <footer class="pt-5 pb-3 bg-dark">
+        <p class="text-center text-muted border-top pt-3 mt-3">© 2023 <a target="_blank" href="https://github.com/anmamebo" class="text-muted">Antonio Manuel Mérida Borrero</a></p>
+    </footer>
 </body>
 
 </html>
