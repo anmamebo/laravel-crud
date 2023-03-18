941fbe4ce237a517dc983f9eba44fa67de18ea01
Antonio Manuel <anmamebo2001@gmail.com>
2023-03-18 13:50:50 +0100

    Reformateado fichero de rutas web.php

--- routes/web.php
+++ routes/web.php
@@ -16,18 +16,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [ TrabajadorsController::class, 'index' ]);
+Route::get('/', [TrabajadorsController::class, 'index'])->name('trabajadores');
 
-Route::get('/empleados', [ EmpleadosController::class, 'index' ]);
-Route::get('/empleados/create', [ EmpleadosController::class, 'create' ])->name('empleados.create');
-Route::post('/empleados/create', [ EmpleadosController::class, 'store' ])->name('empleados.create');
-Route::get('/empleados/edit/{empleado}', [ EmpleadosController::class, 'edit' ])->name('empleados.edit');
-Route::put('/empleados/edit/{empleado}', [ EmpleadosController::class, 'update' ])->name('empleados.edit');
-Route::delete('/empleados/delete/{empleado}', [ EmpleadosController::class, 'destroy' ])->name('empleados.delete');
+Route::get('/empleados', [EmpleadosController::class, 'index'])->name('empleados');
+Route::get('/empleados/create', [EmpleadosController::class, 'create'])->name('empleados.create');
+Route::post('/empleados/create', [EmpleadosController::class, 'store'])->name('empleados.create');
+Route::get('/empleados/edit/{empleado}', [EmpleadosController::class, 'edit'])->name('empleados.edit');
+Route::put('/empleados/edit/{empleado}', [EmpleadosController::class, 'update'])->name('empleados.edit');
+Route::delete('/empleados/delete/{empleado}', [EmpleadosController::class, 'destroy'])->name('empleados.delete');
 
-Route::get('/gerentes', [ GerentesController::class, 'index' ]);
-Route::get('/gerentes/create', [ GerentesController::class, 'create' ])->name('gerentes.create');
-Route::post('/gerentes/create', [ GerentesController::class, 'store' ])->name('gerentes.create');
-Route::get('/gerentes/edit/{gerente}', [ GerentesController::class, 'edit' ])->name('gerentes.edit');
-Route::put('/gerentes/edit/{gerente}', [ GerentesController::class, 'update' ])->name('gerentes.edit');
-Route::delete('/gerentes/delete/{gerente}', [ GerentesController::class, 'destroy' ])->name('gerentes.delete');
+Route::get('/gerentes', [GerentesController::class, 'index'])->name('gerentes');
+Route::get('/gerentes/create', [GerentesController::class, 'create'])->name('gerentes.create');
+Route::post('/gerentes/create', [GerentesController::class, 'store'])->name('gerentes.create');
+Route::get('/gerentes/edit/{gerente}', [GerentesController::class, 'edit'])->name('gerentes.edit');
+Route::put('/gerentes/edit/{gerente}', [GerentesController::class, 'update'])->name('gerentes.edit');
+Route::delete('/gerentes/delete/{gerente}', [GerentesController::class, 'destroy'])->name('gerentes.delete');
