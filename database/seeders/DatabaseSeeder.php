0b7139b54d842aa6746ebe78434930eb6abb9a71
Antonio Manuel <anmamebo2001@gmail.com>
2023-03-24 01:42:46 +0100

    Definido orden de carga de seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,11 +14,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call([
+            PersonasTableSeeder::class,
+            TrabajadoresTableSeeder::class,
+            EmpleadosTableSeeder::class,
+            GerentesTableSeeder::class,
+            TelefonosTableSeeder::class,
+        ]);
     }
 }
